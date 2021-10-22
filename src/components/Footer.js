1844ce5d0e3b0773da18e62470d2c3a55821d0a5
Stackbit Code Editor <projects@stackbit.com>
2021-10-22 01:44:53 +0000

    Footer.js: changed

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -16,12 +16,7 @@ export default class Footer extends React.Component {
         const links = _.get(footer, 'links');
 
         return (
-            <div style="position:relative;z-index:2147483647">
-  <minymon-body enableFeed="true" enableTalk="true" idsString="MWFkZmVkMjctYmM0Ny00ZjgyLWEwMGQtYjI2ODUyOWUzY2JkLmR0YVZOeWd1UG1wRHl5dHpqWFRaZA==" infoTitle="Hi there!" theme="gray">
-    <minymon-feedback description="Your message will be sent to the owner of this site." mid="1adfed27-bc47-4f82-a00d-b268529e3cbd" theme="gray" uid="dtaVNyguPmpDyytzjXTZd">
-    </minymon-feedback>
-  </minymon-body>
-</div>
+            
             <footer className="site-footer">
                 <div className="container container--lg">
                     {((hasNav && !_.isEmpty(navLinks)) || (hasSocial && !_.isEmpty(socialLinks))) && (
