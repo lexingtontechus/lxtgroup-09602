b45c4f0f4f5ffc9c6a1eecf96bc125e23b3c4e6b
Stackbit Code Editor <projects@stackbit.com>
2021-10-22 02:20:46 +0000

    _document.js: changed

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -32,13 +32,12 @@ export default class MyDocument extends Document {
         return (
             <Html {...this.helmetHtmlAttrComponents}>
                 <Head>{this.helmetHeadComponents}
+                    <script id="convertful-api" src="https://app.convertful.com/Convertful.js?owner=6615" async></script>
+                    <script src="https://cdnjs.cloudflare.com/ajax/libs/bodymovin/5.7.8/lottie_svg.min.js" type="text/javascript">
+                    </script>
+                    <script src="https://api.minymon.com/minymon.js" defer type="text/javascript">
+                    </script>
                     
-<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-})(window,document,'script','dataLayer','GTM-KFBK2N3');</script>
-
                 </Head>
                 <body {...this.helmetBodyAttrComponents}>
                     <div class="minymon">
