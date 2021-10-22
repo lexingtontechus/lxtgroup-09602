d7ab838072a618955e6bca8f74fb2b84a00fa252
Stackbit Code Editor <projects@stackbit.com>
2021-10-22 02:23:42 +0000

    _document.js: changed

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -43,7 +43,7 @@ j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
                     
                 </Head>
                 <body {...this.helmetBodyAttrComponents}>
-                    <!-- Google Tag Manager (noscript) -->
+                    
 <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-KFBK2N3"
 height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
 <!-- End Google Tag Manager (noscript) -->
