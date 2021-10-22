bd076f6f0d4aec990ab3cf94cc6af38aa3424298
Stackbit Code Editor <projects@stackbit.com>
2021-10-22 02:20:31 +0000

    _document.js: changed

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -38,7 +38,7 @@ new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
 j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
 'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
 })(window,document,'script','dataLayer','GTM-KFBK2N3');</script>
-<!-- End Google Tag Manager -->
+
                 </Head>
                 <body {...this.helmetBodyAttrComponents}>
                     <div class="minymon">
