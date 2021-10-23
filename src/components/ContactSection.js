36660191ba80c16c0b72d191e5b9cbfb7d1dfcb0
Stackbit Code Editor <projects@stackbit.com>
2021-10-23 01:56:14 +0000

    ContactSection.js: changed

--- src/components/ContactSection.js
+++ src/components/ContactSection.js
@@ -31,7 +31,7 @@ export default class ContactSection extends React.Component {
                                     <option value="">Please select</option>
                                     <option value="Business Inquiries">Business Inquiries</option>
                                     <option value="Partnership">Partnerships</option>
-                                    <option value="Partnership">Partnerships</option>
+                                    <option value="Startup">Partnerships</option>
                                     <option value="Other">Other</option>
                                 </select>
                             </div>
